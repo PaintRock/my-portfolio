0e64b6dc1a4ace6d175bc45414ef2f99a8e3a6c1
Adel Knode <ann.knode@atlasschool.com>
2025-04-23 19:43:30 +0000

    Vercel for the Win

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,118 +1,74 @@
 export const personalInfo = {
-  name: "Rishikesh S",
-  location: "Coimbatore, India",
-  email: "rkesh2003@gmail.com",
-  github: "https://github.com/rishikesh2003",
-  linkedin: "https://www.linkedin.com/in/rishikeshs/",
+  name: "A. Adel Knode ",
+  location: "Tulsa, Oklahoma",
+  email: "adel.knode@gmail.com",
+  github: "https://github.com/PaintRock/TaskCom",
+  linkedin: "https://www.linkedin.com/in/adelknode/",
 };
 
 export const workExperience = [
   {
-    company: "Chatbyte GmbH",
-    location: "Remote",
-    position: "Software Engineer",
-    period: "Mar 2024 - Present",
+    company: "L&J Environmental",
+    location: "Oklahoma",
+    position: "Environmental Manager/Field Technician",
+    period: "Dec 2018 - Present",
     achievements: [
-      "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
-      "Built a comprehensive admin panel for managing content, user data, and analytics.",
-      "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
-      "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
-      "Drove the integration of a custom affiliate marketing system, enabling seamless tracking and reporting of referrals and user activities.",
-      "Integrated Text-to-Speech (TTS) services for enhanced user experiences, optimizing both frontend and backend systems.",
-      "Collaborated with cross-functional teams to deliver high-quality features, actively reviewing pull requests to ensure code quality, adherence to standards, and efficient implementation.",
+      
     ],
   },
-  {
-    company: "DevCrew",
-    location: "Coimbatore, India",
-    position: "Software Engineer",
-    period: "Mar 2023 - Mar 2024",
-    achievements: [
-      "Developed responsive frontend components using React.js, enhancing user experience and app performance.",
-      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
-    ],
-  },
-  {
-    company: "Phoenitags",
-    location: "Coimbatore, India",
-    position: "Software Engineering Intern",
-    period: "Jan 2023 - Mar 2023",
-    achievements: [
-      "Built interactive UIs with React.js, focusing on reusable components and performance optimization.",
-      "Implemented JavaScript-based features like user authentication and real-time updates.",
-    ],
-  },
+  
 ];
 
 export const education = [
   {
-    institution: "Sri Krishna College of Engineering and Technology",
-    location: "Coimbatore, India",
-    degree: "B Tech Information Technology",
-    period: "Jun 2021 - Jun 2025",
-    achievements: [
-      "President of the Department of Information Technology",
-      "G20 student delegate",
-      "Organized multiple technical workshops and events",
-      "Represented my college in multiple national and international hackathons and won.",
-    ],
+    institution: "Atlas School",
+    location: "Tulsa, Okloahoma",
+    degree: "Full-Stack Web Development",
+    period: "Jan 2023 - Apr 2025",
   },
 ];
 export const skills = {
   programmingLanguages: [
     "TypeScript",
     "JavaScript",
-    "Python",
-    "C++",
     "Java",
-    "Solidity",
+
   ],
   frontendDevelopment: [
     "Nextjs",
     "Reactjs",
     "React Native",
-    "Shadcn UI",
     "Tailwind CSS",
     "HTML",
     "CSS",
   ],
   backendDevelopment: ["Nodejs", "Expressjs"],
-  databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
-  cloudAndDevOps: ["AWS"],
-  toolsAndServices: [
-    "Clerk (Auth)",
-    "Sanity (CMS)",
-    "Tinybird (analytics)",
-    "Zod",
-    "Sentry",
-    "Mixpanel",
-    "Trigger.dev",
-  ],
+  databaseAndStorage: ["PostgreSQL",],
+  
 };
 
 export const projects = [
   {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
+    title: "TASKCOM",
+    github: "https://github.com/PaintRock/TaskCom",
     description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+      "TaskCom was born from an urge to help strengthen team communictions for first-responders during training, and missions involving disasters, wildfires, and any movement that involoves mobilizing multiple resources and numerous personnel over a wide area.",
+      "The intent is to create one home page that gives a glimpse of every piece of information needed during an event and the interconnectivity to move quickly to the source of more detailed information.",
+      "The map feature will allow the tracking of all personnel and assets logged into the app and any equipment.",
+      "Icon dropping will enhance the responders ability to convey important information about terrain, structures, and the fire front."
     ],
   },
-  {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
-    description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
-    ],
-  },
+  // {
+  //   title: "Mental Aarog",
+  //   github: "https://github.com/rishikesh2003/mental-aarog",
+  //   description: [
+  //     "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+  //     "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+  //     "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+  //     "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+  //     "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+  //   ],
+  // },
 ];
 
 export const awards = [
